e208ef767e7632761ff5f4fab4aedef2458ef8c0
Nilesh Jaswal <arcanenilesh@gmail.com>
2024-01-10 14:54:25 +0530

    Github live publish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.css";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Navbar from "./components/Navbar";
 import Textform from "./components/Textform";
 import Alert from "./components/Alert";
 // import About from "./components/About";
+// import { BrowserRouter as Router, Routes , Route } from "react-router-dom";
 
 const App = () => {
   const [darkMode, setdarkMode] = useState("light");
@@ -13,39 +14,40 @@ const App = () => {
   const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
+      type: type,
+    });
     setTimeout(() => {
-      setAlert(null)
+      setAlert(null);
     }, 2000);
-  }
+  };
 
   const toggleStyle = () => {
-
     if (darkMode === "dark") {
       setdarkMode("light");
       setButtonText("Dark Mode");
-      showAlert('Dark mode has been Disabled!', "success");
+      showAlert("Dark mode has been Disabled!", "success");
       document.title = "TextUtils";
-    } 
-    
-    else if (darkMode === "light") {
+    } else if (darkMode === "light") {
       setdarkMode("dark");
       setButtonText("Light Mode");
-      showAlert('Dark mode has been Enabled!', "success");
+      showAlert("Dark mode has been Enabled!", "success");
       document.title = "TextUtils - Dark";
     }
-
   };
   return (
-    <div className={`main vh-100 bg-${darkMode} text-${darkMode==='light'?'dark':'light'}`} data-bs-theme={darkMode}>
-      <Navbar title="TextUtils" about="TextUtils about text" darkMode={darkMode} buttonText={buttonText} setButtonText={setButtonText} toggleStyle={toggleStyle}/>
-      <div className="container">
-        <Alert alert={alert}/>
+   /*  <Router> */
+      <div className={`main vh-100 bg-${darkMode} text-${darkMode === "light" ? "dark" : "light"}`} data-bs-theme={darkMode}>
+        <Navbar title="TextUtils" about="About" darkMode={darkMode} buttonText={buttonText} setButtonText={setButtonText} toggleStyle={toggleStyle} />
+        <Alert alert={alert} />
+        <div className="container">
         <Textform showAlert={showAlert} heading="Enter your Text to Analyse" />
-        {/* <About></About> */}
+          {/* <Routes>
+            <Route exact path="/" element={<Textform showAlert={showAlert} heading="Enter your Text to Analyse" />}/>
+            <Route exact path="/about" element={<About />}/>
+          </Routes> */}
+        </div>
       </div>
-    </div>
+    /* </Router> */
   );
 };
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
+// import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-${props.darkMode} bg-${props.darkMode} navbar-expand-lg`}>
       <div className="container">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {props.title}
         </a>
         <button
@@ -22,15 +23,11 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
-                Home
-              </a>
+              <a className="nav-link active" aria-current="page" href="#">Home</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
-                {props.about}
-              </a>
-            </li>
+            {/* <li className="nav-item">
+              <Link  className="nav-link" to="/about">{props.about}</Link>
+            </li> */}
           </ul>
           <input onClick={props.toggleStyle} type="checkbox" className="btn-check" id="btn-check" />
           <label className="btn btn-dark" htmlFor="btn-check">
